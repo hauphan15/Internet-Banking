48088577bc151107d97f26a1fa0f485f62ffc054
hauphan15 <49446469+hauphan15@users.noreply.github.com>
2020-05-08 11:43:39 +0700

    kiểm tra gói tin gửi qua có quá hạn, có bị chỉnh sửa

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 const express = require('express');
+const cors = require('cors');
+const moment = require('moment');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
-const verify = require('./middlewares/partner.mdw');
+
+const verifyPartner = require('./middlewares/partner.mdw');
 
 require('express-async-errors');
 
-///
+
 const app = express();
+app.use(cors());
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
     res.send('Hello from Mrhauphan !!');
 })
 
-app.use('/user-account', verify, require('./routes/UserAccount.route'));
+app.use('/user-account', verifyPartner.partnerCode, verifyPartner.partnerTime, verifyPartner.partnerSig, require('./routes/UserAccount.route'));
 
 app.use((req, res, next) => {
     res.status(404).send('NOT FOUND');
--- middlewares/partner.mdw.js
+++ middlewares/partner.mdw.js
@@ -1,15 +1,52 @@
 const createError = require('http-errors');
 const config = require('../config/default.json');
+const moment = require('moment');
+const CryptoJS = require('crypto-js');
 
-module.exports = function(req, res, next) {
+module.exports = {
 
-    const partnerCode = req.headers['x-partner-code'];
+    partnerCode: (req, res, next) => {
+        const partnerCode = req.headers['x-partner-code'];
+        //so sanh partnerCode
+        if (partnerCode === config.partner.partnerCode) {
+            next();
 
-    if (partnerCode === config.partner.partnerCode) {
-        next();
+        } else {
+            res.send('Invalid partnerCode or partnerCode not found');
+            throw createError(401, 'Invalid partnerCode or partnerCode not found');
+        }
+    },
+    partnerTime: (req, res, next) => {
 
-    } else {
-        res.send('Invalid partnerCode or partnerCode not found');
-        throw createError(401, 'Invalid partnerCode.');
+        const partnerTime = req.headers['x-partner-time'];
+        const now = moment().unix();
+        //kiem tra goi tin da qua han hay chua, chi chap nhan goi tin da gui duoi 60s
+        if ((now - partnerTime) <= 60) {
+            next();
+        } else {
+
+            res.send('Invalid time');
+            throw createError(401, 'Invalid time');
+        }
+    },
+    partnerSig: (req, res, next) => {
+
+        const partnerSig = req.headers['x-partner-sig'];
+        const partnerTime = req.headers['x-partner-time'];
+        const partnerBody = req.body.message;
+        console.log(`partnerBody: ${partnerBody}`);
+        //cong partnertime va bodyjson
+        const data = partnerTime + partnerBody;
+        //roi hash bang MD%
+        const ciphertext = CryptoJS.MD5(data, config.partner.SecretKey).toString();
+        console.log(`ciphertext: ${ciphertext}`);
+        if (partnerSig === ciphertext) {
+            next();
+        } else {
+
+            res.send('Invalid signature');
+            throw createError(401, 'Invalid signature');
+        }
+
     }
 }
