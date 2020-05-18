a31744228f01172045151a74fc84630a6444a37a
zSimpleMan <43574093+zSimpleMan@users.noreply.github.com>
2020-05-18 17:42:02 +0700

    mongodb atlas create User

--- routes/UserAccount.route.js
+++ routes/UserAccount.route.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const userAccountModel = require('../models/UserAccount.model');
+const mongoose = require('mongoose');
+
+const User = require('../DB/User.model');
 
 
 const router = express.Router();
@@ -10,6 +13,29 @@ router.post('/infor', async function(req, res) {
     if (listAcc.length === 0)
         return res.send('UserId not found');
     res.json(listAcc[0]);
+})
+
+router.post('/add', function(req,res){
+    const newUser = new User({
+        id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        money: 0,
+        email: req.body.email,
+        password: req.body.password
+    });
+    newUser.save()
+    .then(result => {
+        console.log(result);
+    }).catch(err =>{
+        console.log(err);
+    })
+
+    res.status(201).json({
+        message: "create successful",
+        user: newUser
+    })
+
+
 })
 
 
