9fc184d8886ca5b5d44dc01e928e1579bddc68a7
hauphan15 <49446469+hauphan15@users.noreply.github.com>
2020-08-19 23:14:01 +0700

    commit

--- app.js
+++ app.js
@@ -47,10 +47,10 @@ app.use('/misspw', require('./routes/login/ForgetPassword.route'));
 
 //-----------------------EMPLOYEE------------------------
 //employee create acc
-app.use('/employee', verifyToken ,require('./routes/employee/CreateCustomerAcc.route'));
+app.use('/employee', verifyToken, require('./routes/employee/CreateCustomerAcc.route'));
 
 //employee transaction
-app.use('/employee',  require('./routes/employee/CustomerTrans.route'));
+app.use('/employee', verifyToken, require('./routes/employee/CustomerTrans.route'));
 
 
 //---------------------CUSTOMER--------------------------
@@ -58,13 +58,13 @@ app.use('/employee',  require('./routes/employee/CustomerTrans.route'));
 app.use('/customer', verifyToken, require('./routes/customer/UserAccount.route'));
 
 //takerlist
-app.use('/customer/takerlist', require('./routes/customer/TakerList.route'));
+app.use('/customer/takerlist', verifyToken, require('./routes/customer/TakerList.route'));
 
 //transaction
-app.use('/customer', require('./routes/customer/Transaction.route'));
+app.use('/customer', verifyToken, require('./routes/customer/Transaction.route'));
 
 //nhắc nợ
-app.use('/customer/reminddebt', require('./routes/customer/RemindDebt.route'));
+app.use('/customer/reminddebt', verifyToken, require('./routes/customer/RemindDebt.route'));
 
 
 //---------------------ADMIN--------------------------
@@ -72,10 +72,10 @@ app.use('/customer/reminddebt', require('./routes/customer/RemindDebt.route'));
 app.use('/admin', verifyToken, require('./routes/administrator/PartnerTrans.route'));
 
 //quản lý nhân viên
-app.use('/admin', require('./routes/administrator/ManageEmployee.route'));
+app.use('/admin', verifyToken, require('./routes/administrator/ManageEmployee.route'));
 
 //tạo tk admin
-app.use('/admin', require('./routes/administrator/Admin.route'));
+app.use('/admin', verifyToken, require('./routes/administrator/Admin.route'));
 
 
 
